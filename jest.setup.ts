138b88d34f5d486548e1e9e27936e91cb1618436
sludwikowski <ludwikowski.szym1993@gmail.com>
2023-10-29 20:45:19 +0100

    Fix eslint warnings

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,5 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-/* eslint-disable @typescript-eslint/no-unsafe-return */
 // Optional: configure or set up a testing framework before each test.
 // If you delete this file, remove `setupFilesAfterEnv` from `jest.config.js`
 import '@testing-library/jest-dom/extend-expect'
